a1551b683d37cf74e63e1ff27633780e5e6e74a6
David Cuthell and Paul Guevarra <david@>
2017-07-13 13:06:18 -0700

    add function to remove spaces and punctuation marks from input string.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,24 @@
+//back end
+// function removePunc(myPhrase){
+//   var length = myPhrase.length;
+//   var output = "";
+//   for(var i = 0; i < length; i++){
+//     if(myPhrase[i] === " " || myPhrase[i] === "'" || myPhrase[i] === "!" || myPhrase[i] === "," || myPhrase[i] === "."){
+//       output = myPhrase.split(0, i);
+//       myPhrase.split(i+1, )
+//     }
+//   }
+//   return myPhrase;
+// }
+
 function toCrypt(myPhrase){
   // console.log("sup");
   var output = "aaa";
-  output = myPhrase.replace(" ", "");
+  output = myPhrase.replace(/[\s\,\'\.\?\!]/g, '');
   return output;
 }
 
-
+//front end
 $(document).ready(function(){
   $("#inputForm").submit(function(event){
     event.preventDefault();
